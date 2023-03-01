36cc5e1851096dad2f11635e583faa97c7beb3ad
hoonkim <hoonk9387@gmail.com>
2023-02-28 21:06:52 -0600

    Added get request for route with parameter '/animal/:name'.

--- main.js
+++ main.js
@@ -2,11 +2,20 @@
 const express = require('express')
 // Create instance of express application.
 const app = express()
+
 // Route '/'.
 // GET request.
 app.get('/', (req, res) => {
     res.send('Hello world')
 })
+// Route '/animal/:name'
+// GET request.
+app.get('/animal/:name', (req, res) => {
+    let animal = req.params.name
+    console.log(animal)
+    res.send("Hello " + animal)
+})
+
 
 // Server listen.
 app.listen(4000)
