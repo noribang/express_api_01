e2d39c94b44074e776250e4f83910015cbed4834
hoonkim <hoonk9387@gmail.com>
2023-02-28 21:16:23 -0600

    Added get request and respond with json.

--- main.js
+++ main.js
@@ -17,6 +17,17 @@ app.get('/animal/:name/:color', (req, res) => {
     res.send("Hello " + animal + color)
 })
 
+app.get("/api/animal/:name", (req, res) => {
+    if (req.params.name === "meowsalot") {
+      res.json({ name: "Meowsalot", species: "cat", "photo": "https://learnwebcode.github.io/json-example/images/cat-1.jpg", bio: "This cat is great and very vocal. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis asperiores, sunt consectetur a amet dolorem rem animi tempore molestias nesciunt fuga, sequi alias voluptatum totam reprehenderit assumenda deleniti distinctio? Cumque." })
+    } else if (req.params.name === "barksalot") {
+      res.json({ name: "Barksalot", species: "dog", "photo": "https://learnwebcode.github.io/json-example/images/dog-1.jpg", bio: "This dog is very communicative. Deleniti, tempora quis commodi qui inventore ratione rem porro doloribus et obcaecati cumque quibusdam voluptatibus iure nisi aut minima consequuntur, officiis esse? Lorem ipsum, dolor sit amet consectetur adipisicing elit." })
+    } else if (req.params.name === "purrsloud") {
+      res.json({ name: "Purrsloud", species: "cat", "photo": "https://learnwebcode.github.io/json-example/images/cat-2.jpg", bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis asperiores, sunt consectetur a amet dolorem rem animi tempore molestias nesciunt fuga, sequi alias voluptatum totam reprehenderit assumenda deleniti distinctio? Cumque. Lorem ipsum." })
+    } else {
+      res.json("Animal not found.")
+    }
+  })
 
 // Server listen.
 app.listen(4000)
